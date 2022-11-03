cd70eac0ed3194a5c6df2ac567cb79c903d93e1a
paulgr98 <pawelwnuk98@gmail.com>
2022-11-03 19:53:51 +0100

    Added login template

---
+++ GamePack.Wpf/Attached Properties/LoginServiceButtons.cs
@@ -0,0 +1,104 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GamePack.Wpf.Attached_Properties
+{
+    class LoginServiceButtons : Button
+    {
+        public PathGeometry Icon
+        {
+            get { return (PathGeometry)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(PathGeometry), typeof(LoginServiceButtons));
+
+
+
+        public ImageSource ImageIcon
+        {
+            get { return (ImageSource)GetValue(ImageIconProperty); }
+            set { SetValue(ImageIconProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageIcon.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageIconProperty =
+            DependencyProperty.Register("ImageIcon", typeof(ImageSource), typeof(LoginServiceButtons));
+
+
+
+        public double IconHeight
+        {
+            get { return (double)GetValue(IconHeightProperty); }
+            set { SetValue(IconHeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconHeight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconHeightProperty =
+            DependencyProperty.Register("IconHeight", typeof(double), typeof(LoginServiceButtons));
+
+
+
+        public double IconWidth
+        {
+            get { return (double)GetValue(IconWidthProperty); }
+            set { SetValue(IconWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconWidthProperty =
+            DependencyProperty.Register("IconWidth", typeof(double), typeof(LoginServiceButtons));
+
+
+
+
+        public Brush IconFill
+        {
+            get { return (Brush)GetValue(IconFillProperty); }
+            set { SetValue(IconFillProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconFill.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconFillProperty =
+            DependencyProperty.Register("IconFill", typeof(Brush), typeof(LoginServiceButtons));
+
+
+
+        public Brush IconFillHover
+        {
+            get { return (Brush)GetValue(IconFillHoverProperty); }
+            set { SetValue(IconFillHoverProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconFillHover.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconFillHoverProperty =
+            DependencyProperty.Register("IconFillHover", typeof(Brush), typeof(LoginServiceButtons));
+
+
+
+        public Brush IconBackground
+        {
+            get { return (Brush)GetValue(IconBackgroundProperty); }
+            set { SetValue(IconBackgroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconBackground.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconBackgroundProperty =
+            DependencyProperty.Register("IconBackground", typeof(Brush), typeof(LoginServiceButtons));
+
+
+
+        public Brush IconBackgroundHover
+        {
+            get { return (Brush)GetValue(IconBackgroundHoverProperty); }
+            set { SetValue(IconBackgroundHoverProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IconBackgroundHover.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IconBackgroundHoverProperty =
+            DependencyProperty.Register("IconBackgroundHover", typeof(Brush), typeof(LoginServiceButtons));
+    }
+}

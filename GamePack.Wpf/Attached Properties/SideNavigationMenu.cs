2dbde18dbc4332a6cecb61797510388552e08a00
UsefulSpider <pawelwnuk98@gmail.com>
2022-11-09 19:40:26 +0100

    Implemented basic side menu navbar

---
+++ GamePack.Wpf/Attached Properties/SideNavigationMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GamePack.Wpf.Attached_Properties
+{
+    class SideNavigationMenu : UserControl
+    {
+        public object MenuButtonsContainer
+        {
+            get => GetValue(MenuButtonsContainerProperty);
+            set => SetValue(MenuButtonsContainerProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MenuButtonsContainer.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MenuButtonsContainerProperty =
+            DependencyProperty.Register("MenuButtonsContainer", typeof(object), typeof(SideNavigationMenu));
+
+        public object FooterMenuButtonsContainer
+        {
+            get => GetValue(FooterMenuButtonsContainerProperty);
+            set => SetValue(FooterMenuButtonsContainerProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for FooterMenuButtonsContainer.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FooterMenuButtonsContainerProperty =
+            DependencyProperty.Register("FooterMenuButtonsContainer", typeof(object), typeof(SideNavigationMenu));
+    }
+}

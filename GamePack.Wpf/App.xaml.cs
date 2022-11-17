67bf45c8d58bf80c81d55b328c2aa9c8e209a783
Grzegorz_Wisniewski <grzechuwisniewski18@gmail.com>
2022-11-17 16:48:26 +0100

    Remove not needed files

--- GamePack.Wpf/App.xaml.cs
+++ GamePack.Wpf/App.xaml.cs
@@ -2,8 +2,6 @@
 using GamePack.Wpf.Factories;
 using GamePack.Wpf.HostExtensions;
 using GamePack.Wpf.Pages;
-using GamePack.Wpf.Stores;
-using GamePack.Wpf.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -44,23 +42,13 @@ namespace GamePack.Wpf
         {
             _host.Start();
 
-            AppDbContext d = _host.Services.GetRequiredService<AppDbContext>();
-
             AppDbContextFactory contextFactory = _host.Services.GetRequiredService<AppDbContextFactory>();
             using (AppDbContext dbContext = contextFactory.CreateDbContext())
             {
                 dbContext.Database.Migrate();
             }
 
-            NavigationStore navigationStore = new NavigationStore();
-
-            navigationStore.CurrentViewModel = new LibraryViewModel(navigationStore);
-
-            MainWindow = new MainWindow(_host.Services.GetRequiredService<IAbstractFactory<LoadingPage>>())
-            {
-                DataContext = new MainViewModel(navigationStore)
-            };
-
+            MainWindow = new MainWindow(_host.Services.GetRequiredService<IAbstractFactory<LoadingPage>>());
             MainWindow.Show();
 
             base.OnStartup(e);
